f3c06553032256c6e0ddcf3ee68ad06b246ccdbc
Mert <uzeyirmertciflikli@gmail.com>
2020-03-22 20:19:05 +0100

    delete button added

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     ]
   };
 
+  // Toggle Complete
   markComplete = id => {
     this.setState({
       todos: this.state.todos.map(todo => {
@@ -35,10 +36,19 @@ class App extends Component {
     });
   };
 
+  // Delete Todo
+  delTodo = id => {
+    console.log(id);
+  };
+
   render() {
     return (
       <div className='App'>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
